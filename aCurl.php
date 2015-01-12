473d863c5e7fc0de6c31e3bbb1daa2d6ce432407
James Kurtz <jkrrv@jkrrv.org>
2015-01-12 15:28:28 -0500

    added versino define

--- aCurl.php
+++ aCurl.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+define('HAS_ACURL_VERSION',1.1);
+
  class aCurl { 
 	protected $_useragent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36'; 
 	protected $_url; 
